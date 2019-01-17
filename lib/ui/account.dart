5ceddacd79a14ebc5d6ec5e4e57a865c04791f1d
nozomi0966 <nozomotoitech@gmail.com>
2019-01-17 10:32:07 +0900

    add home

--- lib/ui/account.dart
+++ lib/ui/account.dart
@@ -10,8 +10,92 @@ class _AccountState extends State<Account> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Account"),
+      backgroundColor: Colors.grey,
+      appBar: AppBar(
+        title: new Text("アカウント"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("note user profile"),
+                leading: CircleAvatar(
+                  ),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("好きを付けたノート"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("プロフィール設定"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("アカウント設定"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("プッシュ通知"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("よくある質問"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("プライバシーポリシー"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("ご利用規約"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("フィードバック"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+            Container(
+              color: Colors.white,
+              child: ListTile(
+                title: Text("ログアウト"),
+                trailing: Icon(Icons.navigate_next),
+              ),
+            ),
+            SizedBox(height: 30.0,),
+          ],
+        ),
       ),
     );
   }
--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -12,8 +12,54 @@ class _HomeState extends State<Home> {
       appBar: AppBar(
         title: new Text("タイムライン"),
       ),
-      body: Center(
-        child: Text("Home"),
+      body: ListView(
+        children: List.generate(10, (index) {
+          return _cardView(index);
+        }),
+      ),
+    );
+  }
+  
+  Widget _cardView(int index) {
+    return Card(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                CircleAvatar(backgroundImage: AssetImage("asset/hoge.png"),),
+                SizedBox(width: 10.0),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("note user $index さん", style: TextStyle(fontWeight: FontWeight.bold),),
+                    Text("not user です")
+                  ],
+                )
+              ],
+            ),
+            SizedBox(height: 5.0,),
+            Image.asset("assets/$index.png", fit: BoxFit.cover, height: 150.0,),
+            SizedBox(height: 10.0,),
+            Text("これはタイトルです$index", style: TextStyle(fontWeight: FontWeight.bold),),
+            SizedBox(height: 10.0,),
+            Text("SQFLite is a Database plugin for flutter. It is highly reliable and embedded Database engine.For CRUD operation we are using async and await. Typically these keywords are used to write asynchronous code.Let’s try one sample to save and retrieve employee data from SQFLite.For that ,we have to import the below dependencies to get it done."),
+            SizedBox(height: 20.0,),
+            Text("スキ   $index件", style: TextStyle(fontWeight: FontWeight.bold),),
+            SizedBox(height: 5.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                Row(mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[Icon(Icons.favorite_border),SizedBox(width: 5.0,), Text("スキ")],),
+                Text("コメントする", style: TextStyle(fontWeight: FontWeight.bold),),
+                Text("シェア", style: TextStyle(fontWeight: FontWeight.bold),),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
